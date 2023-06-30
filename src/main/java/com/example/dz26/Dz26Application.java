5cad76d18d7f459542647d453748d13e91cdb1ca
ааа <https://github.com/@DariyaFernandes/Homework1/pull/1>
2023-06-30 08:58:24 +0300

    dz2-6

---
+++ src/main/java/com/example/dz26/Dz26Application.java
@@ -0,0 +1,12 @@
+package com.example.dz26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz26Application.class, args);
+    }
+}
